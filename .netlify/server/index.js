e126663c1fb7ee09826732e2a4c0a23ace82b97e
ryan-voitiskis <ryanvoitiskis@pm.me>
2023-01-09 02:21:45 +1100

    favicon + netlify.toml

--- .netlify/server/index.js
+++ .netlify/server/index.js
@@ -2423,7 +2423,7 @@ class Server {
       app_template,
       app_template_contains_nonce: false,
       error_template,
-      version: "1673188927384"
+      version: "1673189698138"
     };
   }
   async init({ env }) {
