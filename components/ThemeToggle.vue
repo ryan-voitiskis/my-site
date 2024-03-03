b6559d116ff36a739ad2716253e74b5829f4ed3a
ryan voitiskis <ryanvoitiskis@pm.me>
2024-03-03 13:17:36 +1100

    ThemeToggle uses shad Toggle

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -1,71 +1,14 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+const state = ref(undefined as boolean | undefined)
+
+watch(state, (val) => (colorMode.preference = val ? 'dark' : 'light'))
+onMounted(() => (state.value = colorMode.preference === 'dark' ? true : false))
+</script>
+
 <template>
-	<label class="theme-toggle">
-		<input
-			v-model="$colorMode.preference"
-			type="checkbox"
-			true-value="dark"
-			false-value="light"
-		/>
-		<span class="control" />
-	</label>
+	<Toggle v-model:pressed="state">
+		<IconSun v-if="state === false" class="h-6 w-6" />
+		<IconMoon v-else-if="state === true" class="h-6 w-6" />
+	</Toggle>
 </template>
-
-<style lang="scss">
-.theme-toggle {
-	--toggle-width: 44px;
-	--toggle-height: 28px;
-	--gutter: 2px;
-	--toggle-radius: calc(var(--toggle-height) / 2);
-	--theme-toggle-size: calc(var(--toggle-height) - (var(--gutter) * 2));
-
-	cursor: pointer;
-	display: block;
-	font-size: 22px;
-	padding-left: var(--toggle-width);
-	position: relative;
-	user-select: none;
-
-	input {
-		position: absolute;
-		height: 0;
-		opacity: 0;
-		width: 0;
-	}
-
-	input:checked ~ .control {
-		&:after {
-			left: calc(
-				var(--toggle-width) - var(--theme-toggle-size) - var(--gutter)
-			);
-			background-image: url('/moon.svg');
-		}
-	}
-
-	.control {
-		position: absolute;
-		top: 0;
-		left: 0;
-		height: var(--toggle-height);
-		width: var(--toggle-width);
-		border-radius: calc(var(--toggle-height) / 2);
-		background-color: var(--bg-toggle);
-		transition: background-color 200ms;
-
-		&:after {
-			content: '';
-			background-image: url('/sun.svg');
-			background-size: 24px 24px;
-			background-color: var(--bg);
-			position: absolute;
-			left: var(--gutter);
-			top: var(--gutter);
-			width: var(--theme-toggle-size);
-			height: var(--theme-toggle-size);
-			border-radius: calc(var(--toggle-height) / 2);
-			transition:
-				left 200ms ease-in,
-				background-color 200ms ease-in;
-		}
-	}
-}
-</style>
