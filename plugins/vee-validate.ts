1e0a7f841142503757d7f1e33310a39fef6eb1d3
ryan voitiskis <ryanvoitiskis@pm.me>
2024-03-17 22:32:48 +1100

    cleanup

--- plugins/vee-validate.ts
+++ plugins/vee-validate.ts
@@ -1,7 +1,6 @@
 import { configure } from 'vee-validate'
 
 export default defineNuxtPlugin((nuxtApp) => {
-	// configure vee-validate here
 	configure({
 		validateOnBlur: false,
 		validateOnInput: true
