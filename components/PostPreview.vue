e14be0d492dc1619bba37736a3e927bd4382eee2
ryan voitiskis <ryanvoitiskis@pm.me>
2024-03-23 21:17:22 +1100

    PostPreview grid -> nested flex for medium width screens

--- components/PostPreview.vue
+++ components/PostPreview.vue
@@ -6,43 +6,49 @@ defineProps<{ post: Post }>()
 	<Button
 		as-child
 		variant="blank"
-		class="group mx-auto grid h-auto w-full grid-cols-1 gap-8 overflow-hidden rounded-none border-b p-0 py-12 text-md font-normal text-foreground hover:no-underline focus-visible:ring-4 sm:grid-cols-[256px_288px_minmax(256px,512px)]"
+		class="group flex h-auto w-full flex-col gap-12 overflow-hidden rounded-none border-b p-0 py-12 text-md font-normal text-foreground hover:no-underline focus-visible:ring-4 sm:flex-row"
 	>
 		<a :href="post._path">
 			<img
 				:src="post.image"
 				:alt="post.image_alt"
-				class="transition-hue-rotate-200 mx-auto h-full max-h-64 w-64 max-w-64 rounded-full object-cover sepia transition duration-300 group-hover:sepia-0 group-focus:sepia-0"
+				class="transition-hue-rotate-200 mx-auto h-64 w-64 rounded-full object-cover sepia transition duration-300 group-hover:sepia-0 group-focus:sepia-0"
 			/>
-			<table class="clean mx-auto mb-0 font-mono text-xs text-muted-foreground">
-				<tbody>
-					<tr>
-						<td class="!pl-4">Type</td>
-						<td class="font-thin">{{ post.format }}</td>
-					</tr>
-					<tr>
-						<td class="!pl-4">Published</td>
-						<td class="font-thin">
-							{{ dateStringToLocale(post.published) }}
-						</td>
-					</tr>
-					<tr>
-						<td class="!pl-4">Reading time</td>
-						<td class="font-thin">10 mins</td>
-					</tr>
-				</tbody>
-			</table>
-			<div class="w-full font-sans">
-				<h2
-					class="text-balance underline decoration-transparent transition duration-300 group-hover:decoration-heading-text group-focus:decoration-heading-text"
+			<div
+				class="flex w-full flex-col items-start gap-4 lg:flex-row lg:!items-center lg:justify-items-start lg:gap-12"
+			>
+				<table
+					class="clean mb-0 h-fit w-[260px] shrink-0 font-mono text-xs text-muted-foreground"
 				>
-					{{ post.title }}
-				</h2>
-				<div class="mb-2 flex gap-2">
-					<Badge v-for="(tag, i) in post.tags" :key="i" :text="tag" />
-				</div>
-				<div v-if="post.short" class="w-full">
-					{{ post.short }}
+					<tbody>
+						<tr>
+							<td>Type</td>
+							<td class="font-thin">{{ post.format }}</td>
+						</tr>
+						<tr>
+							<td>Published</td>
+							<td class="font-thin">
+								{{ dateStringToLocale(post.published) }}
+							</td>
+						</tr>
+						<tr>
+							<td>Reading time</td>
+							<td class="font-thin">10 mins</td>
+						</tr>
+					</tbody>
+				</table>
+				<div class="w-full font-sans">
+					<h2
+						class="mb-4 text-balance underline decoration-transparent transition duration-300 group-hover:decoration-heading-text group-focus:decoration-heading-text"
+					>
+						{{ post.title }}
+					</h2>
+					<div class="mb-2 flex flex-wrap gap-2">
+						<Badge v-for="(tag, i) in post.tags" :key="i" :text="tag" />
+					</div>
+					<div v-if="post.short" class="w-full">
+						{{ post.short }}
+					</div>
 				</div>
 			</div>
 		</a>
