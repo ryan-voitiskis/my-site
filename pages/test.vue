08f0f8962532900ea6058fee5f8edce547ba46b1
ryan voitiskis <ryanvoitiskis@pm.me>
2024-03-29 15:09:39 +1100

    attempt netlify form detection with static .html

--- pages/test.vue
+++ pages/test.vue
@@ -24,37 +24,5 @@ const contactForm = ref<InstanceType<typeof ContactForm>>()
 			</div>
 			<ContactForm ref="contactForm" class="w-full max-w-screen-xs" />
 		</div>
-		<form name="test" method="POST" data-netlify="true">
-			<p>
-				<label>
-					Your Name:
-					<input type="text" name="name" />
-				</label>
-			</p>
-			<p>
-				<label>
-					Your Email:
-					<input type="email" name="email" />
-				</label>
-			</p>
-			<p>
-				<label>
-					Your Role:
-					<select name="role[]" multiple>
-						<option value="leader">Leader</option>
-						<option value="follower">Follower</option>
-					</select>
-				</label>
-			</p>
-			<p>
-				<label>
-					Message:
-					<textarea name="message"></textarea>
-				</label>
-			</p>
-			<p>
-				<button type="submit">Send</button>
-			</p>
-		</form>
 	</main>
 </template>
