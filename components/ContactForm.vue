1ef463cf4998c2800c9647d0dede86b83267af92
ryan voitiskis <ryanvoitiskis@pm.me>
2024-03-29 16:22:48 +1100

    netlify form: attempt to use formData directly wo headers

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -26,10 +26,14 @@ function focusNameField() {
 defineExpose({ focusNameField })
 
 const onSubmit = form.handleSubmit(async (values) => {
+	const formData = new FormData()
+	formData.append('form-name', 'contact')
+	Object.entries(values).forEach(([key, value]) => {
+		formData.append(key, value)
+	})
 	const res = await fetch('/', {
 		method: 'POST',
-		headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-		body: new URLSearchParams(values).toString()
+		body: formData
 	})
 	if (res.ok) {
 		toast.success(`thanks for getting in touch ${values.name}!`)
