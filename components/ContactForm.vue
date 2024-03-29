0f3d67dfd9572478b6d62ae0f1e7de1a51a69564
ryan voitiskis <ryanvoitiskis@pm.me>
2024-03-29 14:16:48 +1100

    test attempts at netlify forms

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -45,7 +45,7 @@ const onSubmit = form.handleSubmit(async (values) => {
 </script>
 
 <template>
-	<form netlify @submit="onSubmit">
+	<form name="contact" netlify @submit="onSubmit">
 		<input type="hidden" name="form-name" value="contact" />
 		<FormField v-slot="{ componentField }" name="name">
 			<FormItem class="mb-2">
