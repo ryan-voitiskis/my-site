e67e749daf75c75f2a715795ecedbcf9956b4c34
ryan voitiskis <ryanvoitiskis@pm.me>
2024-03-31 18:54:04 +1100

    migrate: composables + Input, Textarea customisations

--- src/components/ui/input/Input.vue
+++ src/components/ui/input/Input.vue
@@ -2,11 +2,13 @@
 import type { HTMLAttributes } from 'vue'
 import { useVModel } from '@vueuse/core'
 import { cn } from '@/lib/utils'
+import { useValidation } from '@/composables/useValidation'
 
 const props = defineProps<{
-  defaultValue?: string | number
-  modelValue?: string | number
-  class?: HTMLAttributes['class']
+	defaultValue?: string | number
+	modelValue?: string | number
+	class?: HTMLAttributes['class']
+	name: string
 }>()
 
 const emits = defineEmits<{
@@ -17,8 +19,17 @@ const modelValue = useVModel(props, 'modelValue', emits, {
   passive: true,
   defaultValue: props.defaultValue,
 })
+
+const { validationListeners } = useValidation(() => props.name)
 </script>
 
 <template>
-  <input v-model="modelValue" :class="cn('flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50', props.class)">
+  <input v-model="modelValue" 
+		:class="
+			cn(
+				'flex h-9 w-full rounded-md border border-input bg-white/30 px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 dark:bg-muted',
+				props.class
+			)
+		"
+		v-on="validationListeners">
 </template>
