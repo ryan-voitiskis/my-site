28824be224756cb335fb81407fa81e5a55965dcf
ryan voitiskis <ryanvoitiskis@pm.me>
2024-03-31 15:57:16 +1100

    migrate: types + date utils

---
+++ src/lib/date.ts
@@ -0,0 +1,18 @@
+const dateOptions: Intl.DateTimeFormatOptions = {
+	timeZone: 'Australia/Melbourne',
+	year: 'numeric',
+	month: 'long',
+	day: 'numeric'
+}
+
+const timeOptions: Intl.DateTimeFormatOptions = {
+	timeZone: 'Australia/Melbourne',
+	hour: '2-digit',
+	minute: '2-digit',
+	hour12: true
+}
+
+export function dateStringToLocale(date: string): string {
+	const isoDate = new Date(date)
+	return new Intl.DateTimeFormat('en-AU', dateOptions).format(isoDate)
+}
---
+++ src/types.ts
@@ -0,0 +1,37 @@
+// TODO: is there a better solution for this?
+export type ImageAttrs = {
+	src: string
+	alt: string
+}
+
+// TODO: use astro's built in
+export type Post = {
+	_path: string
+	_dir: string
+	_draft: boolean
+	_partial: boolean
+	_locale: string
+	title: string
+	description: string
+	format: string
+	published: string
+	short: string
+	image: string
+	image_alt: string
+	body: any // https://content.nuxt.com/usage/typescript TODO: when we have a better type for this
+	reading_time?: string
+	tags?: string[]
+	_type: string
+	_id: string
+	_source: string
+	_file: string
+	_extension: string
+}
+
+export type Project = {
+	metadata: {
+		title: string
+		url: string
+	}
+	default: string
+}
