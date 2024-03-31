2259571acbf7a8f58580047e630d756d1c33e65d
ryan voitiskis <ryanvoitiskis@pm.me>
2024-03-31 22:04:01 +1100

    feat: ButtonLink component to work around as-child issue

---
+++ src/components/ButtonLink.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { defineProps, onMounted, ref } from 'vue'
+import { Button } from '@/components/ui/button'
+
+defineProps<{
+	href: string
+	label: string
+}>()
+
+const pathname = ref(null as string | null)
+
+onMounted(() => (pathname.value = window.location.pathname))
+</script>
+
+<template>
+	<Button
+		as-child
+		variant="nav-link"
+		class="-my-4 hover:no-underline !decoration-primary !decoration-[0.1rem]"
+		:class="{ '!underline': pathname === href }"
+	>
+		<a :href="href">{{ label }}</a>
+	</Button>
+</template>
