d7addece4fc8592f580f9000b57cc06c73200ad9
ryan voitiskis <ryanvoitiskis@pm.me>
2024-03-31 16:08:47 +1100

    migrate: /projects wip

--- src/components/ui/badge/Badge.vue
+++ src/components/ui/badge/Badge.vue
@@ -6,11 +6,13 @@ import { cn } from '@/lib/utils'
 const props = defineProps<{
   variant?: BadgeVariants['variant']
   class?: HTMLAttributes['class']
+	text?: string
 }>()
 </script>
 
 <template>
   <div :class="cn(badgeVariants({ variant }), props.class)">
+		{{ text }}
     <slot />
   </div>
 </template>
