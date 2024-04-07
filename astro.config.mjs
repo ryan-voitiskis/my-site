1670baddb1f3a623a788697641643fef6addd1b0
ryan voitiskis <ryanvoitiskis@pm.me>
2024-04-07 20:27:43 +1000

    migrate: shiki styling and theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
-import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
+import tailwind from '@astrojs/tailwind'
+import githubLightCustom from './themes/github-light-custom.json'
 
 export default defineConfig({
-  integrations: [vue(), tailwind({
-    applyBaseStyles: false,
-  })],
-  output: 'static'
-});
+	integrations: [
+		vue(),
+		tailwind({
+			applyBaseStyles: false
+		})
+	],
+	output: 'static',
+	markdown: {
+		shikiConfig: {
+			themes: {
+				light: githubLightCustom,
+				dark: 'dracula' // TODO: custom dark
+			}
+		}
+	}
+})
