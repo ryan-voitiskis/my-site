00d402279f4b9df319fdd80dcde080605d65ab33
ryan voitiskis <ryanvoitiskis@pm.me>
2024-04-21 14:20:50 +1000

    feat: prefetchAll: true

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config'
-import vue from '@astrojs/vue'
-import tailwind from '@astrojs/tailwind'
-import githubLightCustom from './themes/github-light-custom.json'
 import mdx from '@astrojs/mdx'
 import netlify from '@astrojs/netlify'
+import tailwind from '@astrojs/tailwind'
+import vue from '@astrojs/vue'
+import { defineConfig } from 'astro/config'
+
+import githubLightCustom from './themes/github-light-custom.json'
 
 export default defineConfig({
+	prefetch: {
+		prefetchAll: true
+	},
 	integrations: [
 		vue(),
 		tailwind({
