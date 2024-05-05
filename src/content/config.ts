abc3fcc4384470dfae9bdd2afb659ddbe44dcc9e
ryan voitiskis <ryanvoitiskis@pm.me>
2024-05-05 23:27:13 +1000

    fix: grammar + improve explanation + cover not required

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,25 +3,30 @@ import { defineCollection, z } from 'astro:content'
 const blogCollection = defineCollection({
 	type: 'content',
 	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			format: z.string(),
-			published: z.string(),
-			updated: z.string().optional(),
-			short: z.string(),
-			cover: image(),
-			coverAlt: z.string(),
-			readingTime: z.string().optional(),
-			images: z
-				.array(
-					z.object({
-						src: z.string(),
-						alt: z.string()
-					})
-				)
-				.optional(),
-			tags: z.array(z.string()).optional()
-		})
+		z
+			.object({
+				title: z.string(),
+				format: z.string(),
+				published: z.string(),
+				updated: z.string().optional(),
+				short: z.string(),
+				cover: image().optional(),
+				coverAlt: z.string().optional(),
+				readingTime: z.string().optional(),
+				images: z
+					.array(
+						z.object({
+							src: z.string(),
+							alt: z.string()
+						})
+					)
+					.optional(),
+				tags: z.array(z.string()).optional()
+			})
+			.refine((data) => !(data.cover && !data.coverAlt), {
+				message: 'coverAlt is required when cover is present',
+				path: ['coverAlt']
+			})
 })
 
 export const collections = {
