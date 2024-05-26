b6fefff10af3a67efa3eb19046307fa9399a743d
ryan voitiskis <ryanvoitiskis@pm.me>
2024-05-26 20:40:47 +1000

    feat: mergeHeadItems + rename interface and vars in head.ts

--- src/lib/head.ts
+++ src/lib/head.ts
@@ -1,4 +1,4 @@
-export interface HeadMeta {
+export interface HeadItems {
 	title?: string
 	meta?: {
 		name?: string
@@ -23,43 +23,75 @@ export interface HeadMeta {
 	}[]
 }
 
-export function renderHeadTags(headMeta: HeadMeta): string {
-	if (!headMeta) return ''
+export function renderHeadItems(headItems: HeadItems[]): string {
+	const items = mergeHeadItems(headItems)
+	// TODO: dedupe meta tags
+	validateHeadItems(items)
+
 	const tags: string[] = []
 
-	if (headMeta.title) tags.push(`<title>${headMeta.title}</title>`)
+	tags.push(`<title>${items.title}</title>`)
 
-	if (headMeta.meta)
-		headMeta.meta.forEach((meta) => {
+	if (items.meta)
+		items.meta.forEach((meta) => {
 			const attrs = Object.entries(meta)
 				.map(([key, value]) => `${key}="${value}"`)
 				.join(' ')
 			tags.push(`<meta ${attrs}>`)
 		})
 
-	if (headMeta.link)
-		headMeta.link.forEach((link) => {
+	if (items.link)
+		items.link.forEach((link) => {
 			const attrs = Object.entries(link)
 				.map(([key, value]) => `${key}="${value}"`)
 				.join(' ')
 			tags.push(`<link ${attrs}>`)
 		})
 
-	if (headMeta.style)
-		headMeta.style.forEach((style) => {
+	if (items.style)
+		items.style.forEach((style) => {
 			tags.push(`<style>${style.cssText}</style>`)
 		})
 
-	if (headMeta.script)
-		headMeta.script.forEach((script) => {
+	if (items.script)
+		items.script.forEach((script) => {
 			const attrs = script.src ? `src="${script.src}"` : ''
 			tags.push(`<script ${attrs}>${script.innerHTML || ''}</script>`)
 		})
 
-	if (headMeta.noscript)
-		headMeta.noscript.forEach((noscript) => {
+	if (items.noscript)
+		items.noscript.forEach((noscript) => {
 			tags.push(`<noscript>${noscript.innerHTML}</noscript>`)
 		})
 
 	return tags.join('\n')
 }
+
+function mergeHeadItems(headItems: HeadItems[]): HeadItems {
+	const mergedHeadItems: HeadItems = {
+		title: '',
+		meta: [],
+		link: [],
+		style: [],
+		script: [],
+		noscript: []
+	}
+
+	headItems.forEach((item) => {
+		if (item.title && item.title.length) mergedHeadItems.title = item.title
+		if (item.meta) mergedHeadItems.meta?.push(...item.meta)
+		if (item.link) mergedHeadItems.link?.push(...item.link)
+		if (item.style) mergedHeadItems.style?.push(...item.style)
+		if (item.script) mergedHeadItems.script?.push(...item.script)
+		if (item.noscript) mergedHeadItems.noscript?.push(...item.noscript)
+	})
+
+	return mergedHeadItems
+}
+
+function validateHeadItems(headItems: HeadItems) {
+	if (!headItems.title || !headItems.title.length)
+		throw new Error('Missing title tag in head.')
+}
+
+// TODO: better error messages - ideally inc page name 'in index.astro' or 'on page /foo'
